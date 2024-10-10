a4d6c81550dd4d3ddb8f998eeb149394e25bb0f6
BlackPri0r <lawrenceli630@gmail.com>
2024-10-11 01:29:27 +1300

    styling fix

--- src/Button.tsx
+++ src/Button.tsx
@@ -6,7 +6,7 @@ type ButtonProps = {
 export default function Button({OnClick, label}: ButtonProps){
   return(
     <div className='text-center'>
-        <button onClick={OnClick} className='bg-orange w-full font-overpass font-bold text-black uppercase tracking-wide pt-2 pb-2 rounded-full hover:bg-white transition'>
+        <button onClick={OnClick} className='bg-orange w-full font-overpass font-bold text-black uppercase tracking-wide pt-2 pb-2 rounded-full hover:bg-white transition duration-200'>
           {label}
         </button>
       </div>
--- src/RatingButton.tsx
+++ src/RatingButton.tsx
@@ -5,7 +5,7 @@ interface buttonProp{
 
 function Button({ OnClick, children }: buttonProp) {
     return (
-      <button onClick={OnClick} className="bg-gray-700 h-14 w-14 rounded-full pt-1 text-light-grey font-overpass font-bold focus:bg-white focus:text-black select-none hover:bg-orange hover:text-black">
+      <button onClick={OnClick} className="bg-gray-700 h-14 w-14 rounded-full pt-1 text-light-grey font-overpass font-bold focus:bg-white focus:text-black select-none hover:bg-orange hover:text-black transition duration-200">
         {children}
       </button>
     );
